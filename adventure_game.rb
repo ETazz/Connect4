e74c9376b22c8f33955288303d52f1c0bb6796b9
ETazz <92504017+ETazz@users.noreply.github.com>
2021-12-16 18:36:13 +1100

    commiting first adventure game

---
+++ adventure_game.rb
@@ -0,0 +1,12 @@
+#items = []
+
+puts ["Welcome to the adventure to Azzara","Over the course of your life you have been told stories of a mystical island in the far distant lands.. This place is everything you imagined.. Quiet literally. Anything you think of appears right in front of you! This beauty is called Azzara. Beautiful isn’t it! Some people in the town you have grown up in don't know wrong from right and believe this mystical island is too good to be true. Well is it? Let’s find out! Time to go on an adventure to Azzara."]
+
+puts "what is your name" 
+name = gets 
+
+puts ["You suddenly wake to a familiar face..", "Good Morning there #{name}!", "Your father is in the training yards, go meet him!"]
+
+
+
+
