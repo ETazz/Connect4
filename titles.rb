42a618fae07a5caaf597d3661b6ece9ebd7144cb
ETazz <92504017+ETazz@users.noreply.github.com>
2021-12-18 08:03:00 +1100

    pushing to github

--- titles.rb
+++ titles.rb
@@ -1,40 +1,7 @@
-#items = []
-#yes = true
-#no = false 
-
-
-prompt = "> "
-
 puts "
 ░█████╗░███████╗███████╗░█████╗░██████╗░░█████╗░
 ██╔══██╗╚════██║╚════██║██╔══██╗██╔══██╗██╔══██╗
 ███████║░░███╔═╝░░███╔═╝███████║██████╔╝███████║
 ██╔══██║██╔══╝░░██╔══╝░░██╔══██║██╔══██╗██╔══██║
 ██║░░██║███████╗███████╗██║░░██║██║░░██║██║░░██║
-╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝" + "\n"
-
-puts ["\n", "Welcome to the adventures of Azzara!!", "\n", "-play", "-settings", "-exit","\n"]
-
-if ARGV.length > 0 
-    flag, *rest = ARGV
-    ARGV.clear
-    case flag
-    when '-settings'
-        puts ['work in progress. other options', '-play', '-exit'] 
-    when '-exit'
-        exit
-    end
-end
-
-while user_input = gets.chomp 
-    case user_input
-when 'play' 
-    require_relative 'game.rb'
-when 'settings'
-    puts ['work in progress', "\n", '-play', '-exit'] 
-when 'exit'
-    exit
-end
-end
-
-
+╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝"
