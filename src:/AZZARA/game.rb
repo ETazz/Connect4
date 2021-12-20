e9efb007a37cc8028b5c6b3750f21ecd8617addd
ETazz <92504017+ETazz@users.noreply.github.com>
2021-12-20 21:30:58 +1100

    unfiinished, needed to hand it in ._.

--- src:/AZZARA/game.rb
+++ src:/AZZARA/game.rb
@@ -1,6 +1,6 @@
 @user = ''
 
-inventory = []
+inventory = ['']
 
 require "tty-prompt"
 
@@ -50,7 +50,7 @@ puts ["\n" + "father: Well #{@user.chomp}, your time has come, i am proud of you
 while answer = prompt.select("\n" + "begin?", [@yesno])
     if answer == "yes"
         puts "\n" + "The first step is through mushy roads! But before you leave, take this bag and sword! You don’t know what is out there! And remember, the stories are true! This is not going to be easy, there will be a lot of challenges and courses to overcome"
-        inventory.append("sword", "bag")
+        inventory.push("sword", "bag")
         break
     elsif answer == "no"
         puts "\n" + "father: what are you waiting for!"
@@ -69,9 +69,8 @@ while answer = prompt.select("\n" + "Hello there young blood, how would you like
 if answer == "Help old lady"
     puts "\n" + "lets catch these kittens"
     puts "\n" + "Old Lady: Thankyou sir! here is a weird fragment thingo for you"
-    inventory.apppend("key-fragment-1")
+    inventory.push("key-fragment-1")
     break
-
     puts "\n" + "#{@user.chomp}: This key fragment seems to make a full key, i need more pieces before it can be used"
     if asnwer == "Don't help"
         puts "\n" + "Old Lady: Darn it you! These old bones aren't made for moving!"
@@ -80,31 +79,68 @@ if answer == "Help old lady"
 end
 
 puts "\n" + "continue with adventure"
-end
+
 puts "\n" + "#{@user.chomp}: Wow this town is amazing! i will have to come back here!"
-end
+
 puts "\n" + "when exiting the town you are greated by a strange man"
-end
+
 puts "\n" + "Hello there, which way are you going? *shaky voice* Left is into the windy woods. If you go there you will certainly be eaten by treacherous creatures.*shaky voice* Right is the great kneeless river! Also known as Fisherman's river. *shaky*"
-end
+
 puts "\n" + "#{@user.chomp}: Thanks for the tip! *walk away*. That mcan seems crazy! What does he know about anything! "
-end
+
 puts "\n" + "when arriving at the split way you try to remember what the crazy man said"
-end
+
 puts "\n" + "#{@user.chomp}: I swear that old man said left is bad and right is good. but they seem to be the other way around!! Right in the woods and left is Fisherman's river.. Argh which way!!"
 
-while answer = promp.select("which way?", ["go right and believe the old man", "go left beacuase it looks right"])
-    if answer = "go right and believe the old man"
+while answer = prompt.select("which way?", ["go right and believe the old man", "go left beacuase it looks right"])
+    if answer == "go right and believe the old man"
         puts "\n" + "continue into the dark woods"
 
+        puts "\n" + "when walking through the woods the crazy man appears again"
+
+        puts "\n" + "#{@user.chomp}: Hows did you get here?!"
+
+        puts "\n" + "crazy man: there are a lot of questions but the real question is why didn't you listen to me?"
+
+        puts "\n" + "suddenly others appear..."
+        
+        while answer = prompt.select("\n" + "#{@user.chomp}: seems to be a trap!", ["turn back and run for your life", "fight"])
+             if answer == "turn back and run for your life"
+                puts "escaped.."
+                break
+                elsif answer == "fight"
+                    puts "\n" + "*slash *slash *slash"
+                    puts "\n" + "item dropped"
+                    puts "\n" + "a key fragment!"
+                    puts "\n" + "continue through forest"
+                    inventory.push "key-fragment-2"
+                    puts "\n" + "arrived at forests end"
         break
-    elsif answer = "go left beacuase it looks right"
+
+    elsif answer == "go left because it looks right"
+        puts "\n" + "continue to river"
+        puts "\n" + "arrive at fishermans land"
+        puts "\n" + "#{@user.chomp}: wow this place is amazing!"
+        while answer = prompt.select("stop and look at store?", ["yes", "no"])
+            if answer == "yes"
+                puts ["sword", "armor", "clothes"]
+                puts "#{@user.chomp}: I cant buy these yet i need to earn some money"
+                puts "left the store..."
+            break
+            elsif answer == "no"
+                puts "continue through fishermans land"
         break 
     end
 end
+end
+end
+end
+end
+end
+
+puts "to be continuued...."
 
 # next code here!!!
-
 
 
 # Error handling
